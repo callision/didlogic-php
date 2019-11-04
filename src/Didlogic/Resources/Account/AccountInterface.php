3d158777b36816d6f8d8638b542252b2a4861aca
Pavel K <pavelsc@gmail.com>
2019-11-04 15:10:49 +0300

    search numbers

--- src/Didlogic/Resources/Account/AccountInterface.php
+++ src/Didlogic/Resources/Account/AccountInterface.php
@@ -4,7 +4,11 @@
 namespace Didlogic\Resources\Account;
 
 
+use Didlogic\Exceptions\RequestException;
+use Didlogic\Exceptions\ResponseException;
 use Didlogic\HttpClient;
+use Didlogic\Resources\Did\Did;
+use Didlogic\Resources\Did\DidList;
 use Didlogic\Resources\ResourceInterface;
 
 class AccountInterface extends ResourceInterface
@@ -15,10 +19,39 @@ class AccountInterface extends ResourceInterface
         parent::__construct($client);
     }
 
+    /**
+     * @param $number
+     * @throws RequestException
+     * @throws ResponseException
+     */
     public function buyNumber($number)
     {
         $options = ["did_numbers" => [$number]];
         $response = $this->client->update('buy/purchase', $options);
+    }
+
+    /**
+     * @param $options
+     *   + Valid arguments
+     *   + [string] number_contains
+     *   + [string] begins_with
+     *   + [string] ends_width
+     *   + [string] city_name_contains
+     *   + [string] country_name_contains
+     *   + [boolean] sms_enabled
+     *   + [integer] page
+     * @return DidList
+     * @throws RequestException
+     * @throws ResponseException
+     */
+    public function searchNumbers(array $options): DidList
+    {
+        $response = $this->client->fetch('buy/search', $options);
+        $dids = [];
+        foreach ($response->getContent()['dids'] as $did) {
+            $dids[] = new Did($this->client, $did);
+        }
+        return new DidList(...$dids);
     }
 
 }
